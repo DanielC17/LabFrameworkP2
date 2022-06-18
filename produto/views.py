e7128b2ca51aeba7ff149df39c3ac9a48eb1c6f7
DanielC17 <danielccabral1981@gmail.com>
2022-06-18 14:28:32 -0300

    add através do ajax

--- produto/views.py
+++ produto/views.py
@@ -19,11 +19,14 @@ def add(request):
         produto.ativo =  request.POST.get('ativo', None)
         produto.imagem = request.POST.get('imagem', None)
         produto.save()
-    return render(request, "produto/form.html")
-
+        lista_produto = Produto.objects.all()
+    return render(request, "produto/resposta.html", context={'lista_produto':lista_produto})
 
 def busca(request):
     if request.method == "GET":
-        busca = request.GET.get('buscar', None)
-        if busca:
-            produtos = Produto.objects.filter(nome=)
+        srch = request.GET.get('src', None)
+        if srch:
+            produtos = Produto.objects.filter(nome=srch)
+        else:
+            produtos = Produto.objects.all()
+    return render(request, "result.html", context={'lista_produto':lista_produto})
